ac465538878600102cc31c91f0fac76691156d7b
DracoR22 <rdraco039@gmail.com>
2024-01-20 14:50:07 -0500

    modal creation

--- client/src/components/molecules/share-document-modal/share-document-modal.tsx
+++ client/src/components/molecules/share-document-modal/share-document-modal.tsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom"
 import useDocument from "../../../hooks/use-document"
-import { useContext, useRef, useState } from "react"
+import { ChangeEvent, useContext, useRef, useState } from "react"
 import { DocumentContext } from "../../../contexts/document-context"
 import useAuth from "../../../hooks/use-auth"
 import validator from "validator"
 import PermissionEnum from "../../../types/enums/permission-enum"
 import DocumentUserService from "../../../services/document-user-service"
+import DocumentUser from "../../../types/interfaces/document-user"
+import { ToastContext } from "../../../contexts/toast-context"
+import DocumentInterface from "../../../types/interfaces/document"
 
 const ShareDocumentModal = () => {
    const { id } = useParams()
@@ -14,6 +17,8 @@ const ShareDocumentModal = () => {
 
    const { document } = useDocument(parseInt(id))
    const { accessToken } = useAuth()
+
+   const { success, error } = useContext(ToastContext)
 
    const [email, setEmail] = useState<null | string>(null)
    const [loading, setLoading] = useState<boolean>(false)
@@ -35,10 +40,53 @@ const ShareDocumentModal = () => {
 
     try {
         const response = await DocumentUserService.create(accessToken, payload)
+        const documentUser = response.data as DocumentUser
+        documentUser.user = { email }
+
+        success(`Succesfully shared dpcument with ${email}`)
+
+        setDocument({...document, users: [...document.users, documentUser]} as DocumentInterface)
+
+        setEmail('')
     } catch (error) {
-        
+        error(`Unable to share this document. Please try again later.`)
+    } finally {
+        setLoading(false)
     }
-   }
+ }
+
+    const handleShareEmailInputChange = (event: ChangeEvent) => {
+        setEmail((event.target as HTMLInputElement).value)
+    }
+
+    const handleCopyLinkBtnClick = () => {
+        if (copyLinkInputRef === null || copyLinkInputRef.current === null) return;
+    
+        const url = window.location.href;
+        copyLinkInputRef.current.value = url;
+        copyLinkInputRef.current.focus();
+        copyLinkInputRef.current.select();
+        window.document.execCommand("copy");
+      };
+    
+      const handleOnKeyPress = async (event: KeyboardEvent) => {
+        if (event.key === "Enter") await shareDocument();
+      };
+    
+      const updateIsPublic = (isPublic: boolean) => {
+        const updatedDocument = {
+          ...document,
+          isPublic: isPublic,
+        } as DocumentInterface;
+    
+        saveDocument(updatedDocument);
+      };
+    
+      const handleShareBtnClick = async () => {
+        await shareDocument();
+      };
+    
+      const alreadyShared = document === null || (document !== null && document.users.filter((documentUser) => documentUser.user.email === email).length > 0);
 
   return (
     <div>ShareDocumentModal</div>
